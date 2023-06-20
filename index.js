7fc7f287e4a4467a4d9c9ed1f050101ba82ec08d
Ariel Torres <42158489+PyKestrel@users.noreply.github.com>
2023-06-20 09:29:35 -0400

    Added IP + Domain Lookup

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 export default {
   async fetch(request, env) {
-    let route = new URL(request.url)
+    let route = new URL(request.url);
     let IP = route.pathname
-
-    return new Response(IP)
+    let url = "https://rdap.arin.net/registry/ip/" + IP.replace(/([()[{*+/$^\\|?])/g, '');
+    const init = {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    };
+    console.log(url)
+    const response = await fetch(url, init);
+    return new Response(response.body)
   }
 }
